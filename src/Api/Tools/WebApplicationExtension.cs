1749fed2d2c988189362fe5d6d6eb99da3a3ce6e
treize <jonathan.rougier@hotmail.fr>
2024-02-03 13:06:54 +0100

    extract configuration for wasm app

--- src/Api/Tools/WebApplicationExtension.cs
+++ src/Api/Tools/WebApplicationExtension.cs
@@ -7,14 +7,13 @@ public static class WebApplicationExtension
 {
     public static void StarterLog(this IApplicationBuilder app)
     {
-
         // var logger = app.ApplicationServices.GetRequiredService<ILogger<WebApplication>>();
         var configuration = app.ApplicationServices.GetRequiredService<IConfiguration>();
         WriteLine("Application started");
         WriteLine("Environment: {0}", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
         WriteLine("Application name: {0}", Assembly.GetEntryAssembly()?.GetName().Name);
         WriteLine("Application version: {0}", Assembly.GetEntryAssembly()?.GetName().Version);
-        WriteLine("Application started at: {0}", DateTime.Now);
+        WriteLine("Application started at: {0:o}", DateTime.UtcNow);
         WriteLine("Database name: {0}", configuration["DbSecret:Database"]);
         WriteLine("Database server: {0}", configuration["DbSecret:Host"]);
     }
